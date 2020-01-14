30b0d635315c5b74cfcaffdf7244b018141b4d98
RaivoJanisGaters <gatersjanis@inbox.lv>
2020-01-14 09:58:58 +0000

    samainiju kodu uz pareizo

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from flask import *
-app=Flask(__name__)
-@app.route('/',methods=["GET"])
+from flask import Flask, render_template
 
-def getIndex():
-  return "<a href='/about'><h1>About</h1></a> <a href='/contact'><h1>Contacts</h1></a> </a>"
+app = Flask(__name__)
 
-@app.route('/about')
+@app.route("/")
+@app.route("/index")
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+@app.route("/about")
 def getAbout():
   return render_template('about.html')
 
-@app.route('/contact')
-def getContact():
-  return render_template('contact.html', phone= 12341234)
+@app.route("/contact")
+def contact():
+  return render_template('contact.html', phone = 87654321)
 
-app.run(host="0.0.0.0",port=8020)
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
